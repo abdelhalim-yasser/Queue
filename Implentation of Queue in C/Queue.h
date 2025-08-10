e020c69f2770526c086e0bd7060acec54e0ca225
Abdelhalim Yasser <ABDELHALIMYASSER88@GMAIL.COM>
2025-08-10 10:46:48 +0300

    Update Queue.h

--- Implentation of Queue in C/Queue.h
+++ Implentation of Queue in C/Queue.h
@@ -1,1 +1,14 @@
+#ifndef QUEUE_H_INCLUDED
+#define QUEUE_H_INCLUDED
+#define MAX 10
 
+typedef struct
+{
+    int front;
+    int rear;
+    int size;
+    entry array[MAX];
+}Queue;
+
+
+#endif // QUEUE_H_INCLUDED
