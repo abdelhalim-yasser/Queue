aa7d682407755910be80842dd1039a19d5a6c232
Abdelhalim Yasser <ABDELHALIMYASSER88@GMAIL.COM>
2025-08-15 17:17:35 +0300

    Update Queue.java

--- Implentation of Queue in Java/Queue.java
+++ Implentation of Queue in Java/Queue.java
@@ -2,3 +2,73 @@ package mypackage;
 import java.util.Random;
 
 
+// Circular Queue Class
+class Queue{
+    protected int size;
+    protected int rear;
+    protected int front;
+    protected int[] array;
+    
+    // Define the Queue
+    public Queue(int size){
+        this.rear = -1;
+        this.front = -1;
+        this.size = size;
+        this.array = new int[size];
+    }
+    
+    // Insertion functions
+    // Queue enqueue function to append the element in the queue
+    public void enqueue(int data){
+        
+    }
+    
+    // Deletion functions
+    // Queue dequeue function to delete the first element in the queue
+    public void dequeue(){
+        
+    }
+    
+    // Queue clear function to clear all the queue elements
+    public void clear(){
+        
+    }
+    
+    // Search functions
+    // Queue getFront to get the front of the Queue
+    public int getFront(){
+        
+    }
+    
+    // Queue getRear to get the rear of the Queue
+    public int getRear(){
+        
+    }
+    
+    // Queue getMiddle to get the middle of the Queue
+    public int getMiddle(){
+        
+    }
+    
+    // Queue getSize to get the size of the Queue
+    public int getSize(){
+        
+    }
+    
+    // Queue getElement to get the specific element from the index in the Queue
+    public int getElement(int index){
+        
+    }
+    
+    // Queue getIndex to get the index of specific element in the Queue
+    public int getIndex(int element){
+        
+    }
+    
+    // Queue isFound to check the Queue's element is found or not
+    public boolean isFound(){
+        
+    }
+    
+    
+}
