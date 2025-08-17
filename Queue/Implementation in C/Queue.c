1510fe4fc063136a074041238fe92fbb41d707ba
Abdelhalim Yasser <ABDELHALIMYASSER88@GMAIL.COM>
2025-08-17 18:21:51 +0300

    Update Queue.c

--- Queue/Implementation in C/Queue.c
+++ Queue/Implementation in C/Queue.c
@@ -1,88 +1,72 @@
-
 #include <stdio.h>
 #include <stdbool.h>
-#define capacity 10
-
-typedef struct{
-    int front;
-    int rear;
-    int capacity;
-    int array[capacity];
-}Queue;
+#include "Queue.h"
 
 // Queue Basic Functions
 // Queue createQueue function to creat the Queue
-void createQueue(&queue){
-    queue->front = 0;
-    queue->rear = -1;
-    queue->size = 0;
+void createQueue(Queue* queue) {
+	queue->front = 0;
+	queue->rear = -1;
+	queue->size = 0;
 }
 
 // Queue isFull function to check if queue is full
-bool isFull(&queue){
-    return queue->size = capacity;
+bool isFull(Queue* queue) {
+	return queue->size == CAPACITY;
 }
 
 // Queue isEmpty function to check if queue is empty
-bool isEmpty(&queue){
-    return size == 0;
+bool isEmpty(Queue* queue) {
+	return queue->size == 0;
 }
 
 // Queue enqueue function that add element in the last index
-void enqueu(int data, &queue){
-    if (isFull()) {
+void enqueue(int data, Queue* queue) {
+	if (isFull(queue)) {
 		printf("Queue overflow.\n");
 		return;
 	}
-    
-    queue->rear++;
-    queue->array[rear] = data;
-    queue->size++;
+
+	queue->rear++;
+	queue->array[queue->rear] = data;
+	queue->size++;
 }
 
 // Queue dequeue function that returns and remove the first element
-void dequeue(&queue){
-    if (isEmpty()) {
+int dequeue(Queue* queue) {
+	if (isEmpty(queue)) {
 		printf("Queue underflow.\n");
-		return;
+		return -1;
 	}
-	
-	int data = array[front];
+
+	int data = queue->array[queue->front];
 	queue->front++;
 	queue->size--;
-	
+
 	return data;
 }
 
 // Queue clear function to clear the Queue
-void clear(&queue){
-    queue->front = queue->size = 0;
-    queue->rear = -1;
+void clear(Queue* queue) {
+	queue->front = 0;
+	queue->rear = -1;
+	queue->size = 0;
 }
 
 // Queue peek function that returns the first(front) element
-void peek(&queue){
-    if (isEmpty())
-		return;
-	
-    return queue->array[front];
+int peek(Queue* queue) {
+	if (isEmpty(queue))
+		return -1;
+
+	return queue->array[queue->front];
 }
-
-
 
 // Queue printQueue function that prints all Queue elements
-void printQueue(&queue){
-    if (isEmpty())
+void printQueue(Queue* queue) {
+	if (isEmpty(queue))
 		return;
-		
+
 	for (int i = queue->front; i <= queue->rear; i++)
-		printf("[%d] -> %d", i, queue->array[i]);
+		printf("[%d] -> %d\n", i, queue->array[i]);
 }
 
-
-int main()
-{
-    printf("Hello World");
-
-    return 0;
-}
