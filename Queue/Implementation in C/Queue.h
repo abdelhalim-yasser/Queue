2d6343c0eeee169b9bc9d6d492cedb1e7bfe6899
Abdelhalim Yasser <ABDELHALIMYASSER88@GMAIL.COM>
2025-08-17 18:28:37 +0300

    Update Queue.h

--- Queue/Implementation in C/Queue.h
+++ Queue/Implementation in C/Queue.h
@@ -1,7 +1,31 @@
-void createQueue(&queue);
-void enqueu(int data, &queue);
-void dequeue(&queue);
-void clear(&queue);
-void peek(&queue);
-void isFull(&queue);
-void isEmpty(&queue);
+#ifndef QUEUE_H_INCLUDED
+#define QUEUE_H_INCLUDED
+
+
+// Queue Basic Functions
+// Queue createQueue function to creat the Queue
+void createQueue(Queue* queue);
+
+// Queue isFull function to check if queue is full
+bool isFull(Queue* queue);
+
+// Queue isEmpty function to check if queue is empty
+bool isEmpty(Queue* queue);
+
+// Queue enqueue function that add element in the last index
+void enqueue(int data, Queue* queue);
+
+// Queue dequeue function that returns and remove the first element
+int dequeue(Queue* queue);
+
+// Queue clear function to clear the Queue
+void clear(Queue* queue);
+
+// Queue peek function that returns the first(front) element
+int peek(Queue* queue);
+
+// Queue printQueue function that prints all Queue elements
+void printQueue(Queue* queue);
+
+
+#endif // QUEUE_H_INCLUDED
